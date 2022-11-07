cde47d74ca9f78eabd182aa07b1ba6d434d34826
lasagne1989 <101409249+lasagne1989@users.noreply.github.com>
2022-11-07 21:49:23 +0000

    initial load

---
+++ gamebutton/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+#libraries
+import json
+from dataclasses import dataclass
+#files
+from modes.standard import Standard
+from modes.nameless import Nameless
+from IPandWebsockets.opensocket import msg, connected
+
+if msg != "next":
+    dict = json.loads(msg)
+
+@dataclass
+class Config:
+    mode: str = dict['mode']
+    time_limit: int = dict['time_limit']
+    players: list = dict['players']
+    dob: list = dict['dob']
+
+def main():
+    while connected == True:
+        if Config.mode == "standard":
+            app = Standard(Config.time_limit, Config.players)
+        if Config.mode == "nameless":
+            app = Nameless(Config.time_limit, Config.players)
+        app.mainloop()
+
+if __name__ == "__main__":
+    main()
+
