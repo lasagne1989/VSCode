6da80a6e64d89d5b4c6ee710e9d25a1d2c8fca91
lasagne1989 <101409249+lasagne1989@users.noreply.github.com>
2022-11-15 15:37:40 +0000

    more less specific imports

--- gamebutton/tools/firstplayer.py
+++ gamebutton/tools/firstplayer.py
@@ -2,14 +2,14 @@
 
 #libraries
 import random
-from data import Data
+import data
 
 def firstPlayer():
-    player_count = len(Data.Config.players)
+    player_count = len(data.Data.Config.players)
     player_num = random.randint(0, player_count - 1)
     player_cycle = []
     for i in range(player_count):
-        player_cycle.append(Data.Config.players[player_num % player_count])
+        player_cycle.append(data.Data.Config.players[player_num % player_count])
         player_num += 1
 
 if __name__ == "__main__":
